f28ab87763d386bfdcdd5094d0a7e7ff584dae75
Ran Wahle <ran.wahle@MBP-RAN.WAHLE>
2022-06-13 22:19:31 +0300

    Correct state

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,46 @@ import './App.css';
 import { Product } from './products';
 import { ProductComponent } from './ProductComponent';
 
-class App extends React.Component<{}, Product[]> {
+class App extends React.Component<{}, {products: Product[]}> {
   addProduct = (product: Product) => {
-    this.setState([...this.state, product]);
+    let {products} = this.state;
+    if (!Array.isArray(products)) {
+      products = [];
+    }
+    this.setState({products: [...products, product]});
+  }
+
+  componentWillMount() {
+    this.setState({products: []})
+    console.log(Date.now(), 'will mount')
+  }
+
+  componentWillUnmount() {
+    console.log(Date.now(), 'will unmount')
+  }
+
+  componentDidMount() {
+    console.log(Date.now(), 'did unmount')
+
+  }
+
+  componentWillUpdate() {
+    console.log(Date.now(), 'will update')
+  }
+
+  componentDidUpdate() {
+    console.log(Date.now(), 'did update', this.state)
+
   }
 
   render() {
+     let {products} = this.state;
+    if (!Array.isArray(products)) {
+      products = [];
+    }
   return (
     <div className="App">
-      {this.state.map(p => <ProductComponent {...p} ></ProductComponent>)
+      {products.map(p => <ProductComponent {...p} ></ProductComponent>)
       }
       <AddProduct onAddProduct={this.addProduct}></AddProduct>
     </div>
