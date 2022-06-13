ab1d630f35319431cdf514b801334b91f11f2d8b
Ran Wahle <ran.wahle@MBP-RAN.WAHLE>
2022-06-13 23:16:52 +0300

    migrate to functional component

--- src/App.tsx
+++ src/App.tsx
@@ -4,58 +4,6 @@ import './App.css';
 import { Product } from './products';
 import { ProductComponent } from './ProductComponent';
 
-// class App extends React.Component<{}, {products: Product[]}> {
-//   addProduct = (product: Product) => {
-//     let {products} = this.state;
-//     if (!Array.isArray(products)) {
-//       products = [];
-//     }
-//     this.setState({products: [...products, product]});
-//   }
-
-//   constructor() {
-//     super({})
-//   }
-
-//   componentWillMount() {
-//     this.setState({products: []})
-//     console.log(Date.now(), 'will mount')
-//   }
-
-//   componentWillUnmount() {
-//     console.log(Date.now(), 'will unmount')
-//   }
-
-//   componentDidMount() {
-//     console.log(Date.now(), 'did unmount')
-
-//   }
-
-//   componentWillUpdate() {
-//     console.log(Date.now(), 'will update')
-//   }
-
-//   componentDidUpdate() {
-//     console.log(Date.now(), 'did update', this.state)
-
-//   }
-
-//   render() {
-//      let {products} = this.state;
-//     if (!Array.isArray(products)) {
-//       products = [];
-//     }
-//   return (
-//     <div className="App">
-//       {products.map(p => <ProductComponent {...p} ></ProductComponent>)
-//       }
-//       <AddProduct onAddProduct={this.addProduct}></AddProduct>
-//     </div>
-//   );
-//     }
-// }
-
-
 function App() {
   const [products, setProducts] = useState<Product[]>([]);
 
