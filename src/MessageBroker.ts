f7f34050f0c5a52328a149724b5d0781ff13b880
Ran Wahle <ran.wahle@MBP-RAN.WAHLE>
2022-06-15 11:24:24 +0300

    Merge useEffect

---
+++ src/MessageBroker.ts
@@ -0,0 +1,23 @@
+export type MessageCallback = (message: string) => void;
+export class MessageBroker {
+
+    callbacks: MessageCallback[] = [];  
+
+    subscribe(messageCallback:MessageCallback) {
+        this.callbacks.push(messageCallback);
+    }
+
+    unsubscribe(messageCallback: MessageCallback) {
+            this.callbacks = this.callbacks.filter(cb => cb !== messageCallback);
+    }
+
+    sendMessage = () => {
+        const message = `Now is ${new Date()}`;
+        console.log(`There are ${this.callbacks.length} callbacks`);
+        this.callbacks.forEach(callback => callback(message) )
+    }
+
+    constructor() {
+        setInterval(this.sendMessage, 1000);
+    }
+}
