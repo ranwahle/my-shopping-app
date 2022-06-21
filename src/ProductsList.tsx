447af298eaae56615fa59f64d3e9e9eed846e511
Ran <ran.wahle@gmail.com>
2022-06-21 23:15:10 +0300

    Simple usememo

--- src/ProductsList.tsx
+++ src/ProductsList.tsx
@@ -3,32 +3,31 @@ import AddProduct from './AddProduct';
 import ProductComponent from './ProductComponent';
 import { Product } from './products';
 
+const TextNumbers: {[index: number]  : string} = {
+  1: 'One', 
+  2: 'Two',
+  3: 'Three',
+  4: 'Four',
+  5: 'Five',
+  6: 'Six',
+  7: 'Seven',
+  8: 'Eight',
+  9: 'Nine',
+  10: 'Ten'
 
+}
 
 function ProductsList(props: {
     deleteProduct: (p: Product) => void,
     products: Product[], addProduct: (product: Product) => void}) {
     const {products, addProduct, deleteProduct} = props;
     console.log('I am in memo but runs', products);
-    const [sumFibunatchi, setSumFibunatchi] = useState(0);
-
-    function fibunatchi() {
-      setSumFibunatchi(fibunatchiIndex(products.length));
+   
+    const listHeader: () => string  = () => {
+      return  TextNumbers[products.length];
     }
-    function fibunatchiIndex(index: number): number {
-      if (index <= 0) {
-        return 0;
-      } 
-      if (index === 1) {
-        return 1;
-      }
-      return index + fibunatchiIndex(index-1) + fibunatchiIndex(index-2);
-    }
-
-    useMemo(fibunatchi, [products.length]);
-   // productFibunatchi();
     return (<>
-    {sumFibunatchi}
+    {useMemo(listHeader, [products.length])} products
      <div className="App">
             <AddProduct onAddProduct={addProduct}></AddProduct>
 
