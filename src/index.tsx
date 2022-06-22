67d9282892be02ad4de552e9294feadf1d9b9c5a
Ran Wahle <ran.wahle@MBP-RAN.WAHLE>
2022-06-22 09:15:10 +0300

    reduc

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createStore} from 'redux';
+import { rootReducer } from './root-reducser';
+import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+const store = createStore(rootReducer)
+
 root.render(
+  <Provider store={store}>
+
     <App />
+    </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
